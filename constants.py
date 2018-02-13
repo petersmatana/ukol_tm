37246e17127ea9b5c5fba37259e73789aa887b01
Peter Smatana <petersmatana@gmail.com>
2018-02-13 22:30:44 +0100

    constants for clarity

---
+++ constants.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+PROPERTY_ID = "ID"
+PROPERTY_PARENT_ID = "Parent-ID"
+PROPERTY_DEPARTMENT_NAME = "DepartmentName"
+PROPERTY_DEPARTMENT_CITY = "DepartmentCity"
+PROPERTY_FIRSTNAME = "Firstname"
+PROPERTY_SURNAME = "Surname"
+PROPERTY_DEPARTMENT_ID = "Department-ID"
+PROPERTY_BIRTHDATE = "BirthDate"
